8162fc8f406a66e0b9b5b1b2bdc1feaf5a16e904
Dhanushsp <sddhanushdp@gmail.com>
2026-08-09 15:21:09 +0530

    fix 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-    prerender: {
-      enabled: true,
-      crawlLinks: true,
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
 });
